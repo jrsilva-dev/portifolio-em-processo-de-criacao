7793e3ed08267812436d95b54e7b052475553556
Jr. Silva <jrsilvablk@gmail.com>
2025-09-23 19:22:02 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+document.querySelectorAll('nav a').forEach(anchor => {
+      anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href');
+        const targetElement = document.querySelector(targetId);
+        window.scrollTo({
+          top: targetElement.offsetTop - 80,
+          behavior: 'smooth'
+        });
+      });
+    });
